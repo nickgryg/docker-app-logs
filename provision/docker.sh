1412a868148e67dcbd4e21dfbe421df43a274bf7
Nickolay Grygoriev <nickgryg@gmail.com>
2017-11-13 00:29:49 +0200

    Add docker-compose

--- provision/docker.sh
+++ provision/docker.sh
@@ -12,3 +12,7 @@ usermod -aG docker vagrant
 
 systemctl enable docker
 systemctl start docker
+
+# Install docker-compose
+curl -sL https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
